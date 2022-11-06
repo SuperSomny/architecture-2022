6ce4fdf9e465d56ba2ee4b37235f7c91dc0a3cec
Somny <1474659519@qq.com>
2022-11-06 10:54:34 +0800

    编写了代码生成模块，但还没测试

--- hw4/ir/ir.py
+++ hw4/ir/ir.py
@@ -4,16 +4,18 @@ class Program:
         self.instrList = []     #指令列表，其中的元素或者是Instruction，或者是Loop
 
     #将中间表示转化为代码并输出到文件
-    def emit(self):
-        #TODO
-        pass
+    def emit(self, stream, cnt):
+        for instr in self.instrList:
+            instr.emit(stream, cnt)
+
+        return cnt
 
 #循环类
 class Loop:
-    def __init__(self, iterator, times, body):
+    def __init__(self, iterator, times):
         self.iter = iterator    #循环变量，是一个寄存器
         self.times = times      #循环次数，是一个寄存器
-        self.body = body        #循环代码，是一个Program，应该有如下的形式
+        self.instrList = []     #指令列表，其中的元素是Instruction或Loop，应该有如下的形式
         '''
         beq i, n, out
         loop:
@@ -23,37 +25,49 @@ class Loop:
         out:
         '''
 
-    def emit(self):
-        #TODO
-        pass
+    def emit(self, stream, cnt):
+        cnt = cnt + 1
+        for i in range(len(self.instrList)):
+            if i == 0:
+                self.instrList[0].emit(stream, cnt)
+                stream.write('loop' + str(cnt) + ':\n')
+            else:
+                self.instrList[i].emit(stream, cnt)
+
+        stream.write('out' + str(cnt) + ':\n')
+        return cnt
 
 #指令类
 class Instruction:
     def __init__(self):
-        self.rs = []    #源寄存器，是一个列表，其中的元素是该指令读取的所有寄存器号
-                        #注意这个列表里去掉了重复的元素
-        self.rt = None  #目的寄存器，是该指令写入的寄存器号
+        self.rs = []            #源寄存器，是一个列表，其中的元素是该指令读取的所有寄存器号
+                                #注意这个列表里去掉了重复的元素
+        self.rt = None          #目的寄存器，是该指令写入的寄存器号
+        self.instrStr = None    #指令字符串
 
-    def emit(self):
+    def emit(self, stream, cnt):
         pass
 
+#存储访问指令类
+class MemInstr(Instruction):
+    def emit(self, stream, cnt):
+        stream.write(self.instrStr + '\n')
+        return cnt
+
 #Load
-class Load(Instruction):
-    def __init__(self, reg1, imm, reg2):
-        #load reg1, imm(reg2)
+class Load(MemInstr):
+    #load reg1, offset(reg2)
+    def __init__(self, reg1, offset, reg2):
         super().__init__()
         self.rt = reg1
         self.rs.append(reg2)
-        self.offset = imm
-
-    def emit(self):
-        #TODO
-        pass
+        self.offset = offset
+        self.instrStr = 'load r' + str(reg1) + ', ' + str(offset) + '(r' + str(reg2) + ')'
 
 #Store
-class Store(Instruction):
-    def __init__(self, reg1, imm, reg2):
-        #store reg1, imm(reg2)
+class Store(MemInstr):
+    #store reg1, imm(reg2)
+    def __init__(self, reg1, offset, reg2):
         super().__init__()
 
         #去掉重复的元素
@@ -61,30 +75,26 @@ class Store(Instruction):
         if reg1 != reg2:
             self.rs.append(reg2)
 
-        self.offset = imm
-
-    def emit(self):
-        #TODO
-        pass
+        self.offset = offset
+        self.instrStr = 'store r' + str(reg1) + ', ' + str(offset) + '(r' + str(reg2) + ')'
 
 #ALU型指令类
 class AluInstr(Instruction):
-    def emit(self):
-        pass
+    def emit(self, stream, cnt):
+        stream.write(self.instrStr + '\n')
+        return cnt
 
 #Nop
 class Nop(AluInstr):
+    #nop
     def __init__(self):
         super().__init__()
-
-    def emit(self):
-        #TODO
-        pass
+        self.instrStr = 'nop'
 
 #Add
 class Add(AluInstr):
+    #add reg1, reg2, reg3
     def __init__(self, reg1, reg2, reg3):
-        #add reg1, reg2, reg3
         super().__init__()
         self.rt = reg1
 
@@ -93,32 +103,26 @@ class Add(AluInstr):
         if reg2 != reg3:
             self.rs.append(reg3)
 
-    def emit(self):
-        #TODO
-        pass
+        self.instrStr = 'add r' + str(reg1) + ', r' + str(reg2) + ', ' + str(reg3)
 
 #Addi
 class Addi(AluInstr):
+    #addi reg1, reg2, imm
     def __init__(self, reg1, reg2, imm):
-        #addi reg1, reg2, imm
         super().__init__()
         self.rt = reg1
         self.rs.append(reg2)
         self.imm = imm
-
-    def emit(self):
-        #TODO
-        pass
+        self.instrStr = 'addi r' + str(reg1) + ', r' + str(reg2) + ', ' + str(imm)
 
 #条件跳转指令类
 class Condition(Instruction):
-    def emit(self):
-        pass
+    pass
 
 #Bne
 class Bne(Condition):
+    #bne reg1, reg2, label
     def __init__(self, reg1, reg2):
-        #bne reg1, reg2, label
         #Bne对象的属性中不需要包含label，它应该在将中间表示转化为代码时被加上
         super().__init__()
 
@@ -127,14 +131,18 @@ class Bne(Condition):
         if reg1 != reg2:
             self.rs.append(reg2)
 
-    def emit(self):
-        #TODO
-        pass
+        self.instrStr = 'bne r' + str(reg1) + ', r' + str(reg2)
+
+    def emit(self, stream, cnt):
+        instrStr = self.instrStr + ', out' + str(cnt)
+        stream.write(instrStr + '\n')
+
+        return cnt
 
 #Beq
 class Beq(Condition):
+    #beq reg1, reg2, label
     def __init__(self, reg1, reg2):
-        #beq reg1, reg2, label
         super().__init__()
 
         #去掉重复的元素
@@ -142,6 +150,12 @@ class Beq(Condition):
         if reg1 != reg2:
             self.rs.append(reg2)
 
-    def emit(self):
-        #TODO
-        pass
+        self.instrStr = 'beq r' + str(reg1) + ', r' + str(reg2)
+
+    def emit(self, stream, cnt):
+
+    def emit(self, stream, cnt):
+        instrStr = self.instrStr + ', loop' + str(cnt)
+        stream.write(instrStr + '\n')
+
+        return cnt
--- hw4/ir/trans.py
+++ hw4/ir/trans.py
@@ -7,5 +7,5 @@ from ir.ir import *
 #效果：
 #   将ir对应的汇编代码写入fileName文件，若文件不存在则创建
 def ir2code(ir, fileName):
-    #TODO
-    pass
+    fileStream = open(fileName, 'w')
+    ir.emit(fileStream)
